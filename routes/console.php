641113f73305ddef71a868282dc6479d96e340c6
Alhilal Anwar <alhilalanwar07@gmail.com>
2026-03-12 05:20:03 +0800

    feat: add Supervisor config & Laravel scheduler for queue worker

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Process queued jobs every minute (alternative to Supervisor)
+Schedule::command('queue:work --stop-when-empty --tries=2 --timeout=180')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->runInBackground();
