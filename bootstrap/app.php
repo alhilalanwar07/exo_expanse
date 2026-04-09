3be632101b513743fb157119fef76d572d696dfc
Alhilal Anwar <alhilalanwar07@gmail.com>
2026-04-09 15:06:17 +0800

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -18,21 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
-
-        $middleware->cors(
-            paths: ['api/*', 'sanctum/csrf-cookie'],
-            allowedMethods: ['*'],
-            allowedOrigins: [
-                'http://localhost:8081',
-                'http://127.0.0.1:8081',
-                'https://exoinvite.site',
-                '*' // Tetap sertakan wildcard untuk mobile native
-            ],
-            allowedHeaders: ['*'],
-            exposedHeaders: [],
-            maxAge: 0,
-            supportsCredentials: true
-        );
 
         $middleware->append([
             ForceHttpsInProduction::class,
