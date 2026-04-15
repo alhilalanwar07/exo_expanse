4901e5d7c1384f75fc16ecf3549a50cce947673e
Alhilal Anwar <alhilalanwar07@gmail.com>
2026-04-15 23:28:09 +0800

    update

--- config/analytics.php
+++ config/analytics.php
@@ -5,14 +5,16 @@ return [
     /*
      * The property id of which you want to display data.
      */
-    'property_id' => env('ANALYTICS_PROPERTY_ID'),
+    'property_id' => env('GOOGLE_ANALYTICS_PROPERTY_ID', env('ANALYTICS_PROPERTY_ID')),
 
     /*
      * Path to the client secret json file. Take a look at the README of this package
      * to learn how to get this file. You can also pass the credentials as an array
      * instead of a file path.
      */
-    'service_account_credentials_json' => storage_path('app/analytics/service-account-credentials.json'),
+    'service_account_credentials_json' => env('GOOGLE_ANALYTICS_CREDENTIALS_BASE64') 
+        ? json_decode(base64_decode(env('GOOGLE_ANALYTICS_CREDENTIALS_BASE64')), true) 
+        : storage_path('service-account-credentials.json'),
 
     /*
      * The amount of minutes the Google API responses will be cached.
